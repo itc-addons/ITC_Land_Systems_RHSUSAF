ab0a0003cb7c0f0c2c8d598620bf83ab78aded2f
Yannik Hegge <yannikhegge@gmail.com>
2018-08-14 00:34:10 +0200

    M119 stuff

---
+++ Addons/itc_land_rhsusaf_m119/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+ class itc_land_rhsusaf_m119	{
+   units[] = {	 };
+   weapons[] = {};
+   requiredVersion = 1.8;
+   requiredAddons[] = {"rhsusf_main", "rhsusf_c_troops", "rhsusf_c_heavyweapons","rhsusf_c_m119","itc_land_veh_sights","itc_land_sphammohandler"};
+   author = "RHS + ITC Addons Team";
+   authors[] = {"RHS","ToadBall","Yax"};
+ };
+};
+
+#include "config\cfgVehicles.hpp"
