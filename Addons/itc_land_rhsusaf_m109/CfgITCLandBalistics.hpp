00b9f940cdd64e49fe04c8c1f886170fd0cb31ee
Andrew Somerville <asmsandrew@googlemail.com>
2018-09-29 17:15:33 +0100

    Smoke added to battery ammunition list

--- Addons/itc_land_rhsusaf_m109/CfgITCLandBalistics.hpp
+++ Addons/itc_land_rhsusaf_m109/CfgITCLandBalistics.hpp
@@ -3,7 +3,7 @@ class itc_land_ballistics {
     class M284 {
       displayName = "M284 155mmm Howitzer";
       type = "cannon";
-      ammunition[] = {"itc_land_g155hex","itc_land_g155icm","itc_land_g155mat","itc_land_g155map","itc_land_g155pgm","itc_land_g155lgm","itc_land_g155ill","itc_land_g155ext"};
+      ammunition[] = {"itc_land_g155hex","itc_land_g155smo","itc_land_g155icm","itc_land_g155mat","itc_land_g155map","itc_land_g155pgm","itc_land_g155lgm","itc_land_g155ill","itc_land_g155ext"};
       weaponClass = "itc_land_rhs_weap_m284";
     };
   };
