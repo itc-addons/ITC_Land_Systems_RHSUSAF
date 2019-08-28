19b39daaec7e8847a0b2ffa78e0353f43833ce50
Andrew Somerville <asmsandrew@googlemail.com>
2019-08-28 23:40:26 +0100

    Disabled artillery computer on M119s

--- Addons/itc_land_rhsusaf_m119/CfgVehicles.hpp
+++ Addons/itc_land_rhsusaf_m119/CfgVehicles.hpp
@@ -7,10 +7,10 @@ class CfgVehicles {
 			class MainTurret: NewTurret {};
 		};
 	};
-	class StaticCannon: StaticWeapon {	
+	class StaticCannon: StaticWeapon {
 		class Turrets: Turrets {
 			class MainTurret: MainTurret {};
-		};	
+		};
 	};
 	class RHS_M119_base: StaticCannon {
 		class Turrets: Turrets {
@@ -20,30 +20,32 @@ class CfgVehicles {
 	class RHS_M119_D: RHS_M119_base	{
 		class Turrets: Turrets {
 			class MainTurret: MainTurret {};
-		};		
+		};
 	};
 	class itc_land_rhsusf_m119_d: RHS_M119_D {
 		displayname = "M119A2 mod. 0";
 		scopeCurator = 2;
+		artilleryScanner = 0;
 		class Turrets: Turrets {
 			class MainTurret: MainTurret {
-				turretInfoType = "ITC_Land_RscGunnerSightBasic";				
+				turretInfoType = "ITC_Land_RscGunnerSightBasic";
 			};
-		};			
+		};
 	};
-	
+
 	class RHS_M119_WD: RHS_M119_base	{
 		class Turrets: Turrets {
 			class MainTurret: MainTurret {};
-		};		
+		};
 	};
 	class itc_land_rhsusf_m119_wd: RHS_M119_WD {
 		displayname = "M119A2 mod. 0";
 		scopeCurator = 2;
+		artilleryScanner = 0;		
 		class Turrets: Turrets {
 			class MainTurret: MainTurret {
-				turretInfoType = "ITC_Land_RscGunnerSightBasic";				
+				turretInfoType = "ITC_Land_RscGunnerSightBasic";
 			};
-		};			
-	};	
+		};
+	};
 };
